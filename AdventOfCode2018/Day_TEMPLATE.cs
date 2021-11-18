a6d53f5844ec751827541b56b2d0b465ac2c0306
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-11-18 15:36:28 +0100

    Add Template day class for ease of use.

--- AdventOfCode2018/Day_TEMPLATE.cs
+++ AdventOfCode2018/Day_TEMPLATE.cs
@@ -2,33 +2,20 @@
 
 namespace AdventOfCode2018
 {
-	public class Day1
+	public class Day_template
 	{
 
 		private DateTime startTime;
 		private TimeSpan duration;
 
-		int[] data;
-		const int SIZE = 4096;
-
-		public Day1()
+		public Day_template()
 		{
 			// Set Data
-			data = new int[SIZE * SIZE];
 
 			// Start Timer
 			startTime = DateTime.Now;
 
 			// Do Code Stuff
-			for( int x = 0; x < SIZE; x++ )
-			{
-				for( int y = 0; y < SIZE; y++ )
-				{
-					int index = x * SIZE + y;
-
-					data[index] = 1;
-				}
-			}
 
 			// Get Duration
 			duration = DateTime.Now - startTime;
